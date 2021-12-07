f150f877bc9af88207d7543a3df1e543bc90b531
Neeraj Tadhiyal <tadhiyalne@gmail.com>
2021-12-07 12:02:31 +0530

    initial commit

---
+++ first.java
@@ -0,0 +1,8 @@
+class neeraj{
+    public static void main(String[] args) {
+        int a = 2;
+        int  b = 2;
+        int c = a+b;
+        System.out.println(c+"  is the sum of"+a+" and "+b);
+    }
+}
